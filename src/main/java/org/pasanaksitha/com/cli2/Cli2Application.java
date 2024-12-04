3cf11d5634577b5732859caa949d1fb72c719a00
Pasan Laksitha <29460924+Pasanlaksitha@users.noreply.github.com>
2024-12-04 23:17:00 +0530

    maan

---
+++ src/main/java/org/pasanaksitha/com/cli2/Cli2Application.java
@@ -0,0 +1,16 @@
+package org.pasanaksitha.com.cli2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class Cli2Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		//SpringApplication.run(Cli2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/pasanaksitha/com/cli2/Cli2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pasanaksitha.com.cli2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cli2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
